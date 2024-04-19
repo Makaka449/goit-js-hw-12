eda80526b6630928830842b3ab9a704c83449859
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-04-19 22:50:03 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import {
-  renderMarkup,
+  renderMarcup,
   showEndOfListMessage,
   showEmptyInputMessage,
   noImagesMessage,
