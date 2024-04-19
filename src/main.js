fd2c13387eaa9e870d19607a8f95fe69eb08fbce
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-04-19 22:48:51 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import axios from 'axios';
+import { fetchImages } from './js/pixabay-api.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+import {
+  renderMarkup,
+  showEndOfListMessage,
+  showEmptyInputMessage,
+  noImagesMessage,
+} from './js/render-functions.js';
 
 const lightbox = new SimpleLightbox('.gallery a', {
   nav: true,
@@ -39,14 +46,14 @@ async function onSubmit(event) {
 
   try {
     const images = await fetchImages(searchWord, currPage).then(data => {
-      const marcup = renderMarcup(data);
+      const markup = renderMarkup(data);
       if (data.hits.length === 0) {
         noImagesMessage();
         loadMoreBtn.style.display = 'none';
         loader.style.display = 'none';
         return;
       }
-      container.insertAdjacentHTML('beforeend', marcup);
+      container.insertAdjacentHTML('beforeend', markup);
       lightbox.refresh();
       loader.style.display = 'none';
     });
@@ -60,8 +67,8 @@ async function onLoadMore() {
   currPage += 1;
   try {
     const images = await fetchImages(searchWord, currPage).then(data => {
-      const marcup = renderMarcup(data);
-      container.insertAdjacentHTML('beforeend', marcup);
+      const markup = renderMarkup(data);
+      container.insertAdjacentHTML('beforeend', markup);
       lightbox.refresh();
 
       const cardHeight = container.getBoundingClientRect().height;
@@ -80,77 +87,3 @@ async function onLoadMore() {
     console.error('Error:', error);
   }
 }
-
-export async function fetchImages(searchWord, currPage) {
-  const KEY = '43280076-efaf032a147c4a401dc5ab87e';
-  const URL = 'https://pixabay.com/api/';
-  const resp = await axios.get(URL, {
-    params: {
-      key: KEY,
-      q: searchWord,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: currPage,
-      per_page: 15,
-    },
-  });
-  return resp.data;
-}
-
-function renderMarcup(data) {
-  return data.hits
-    .map(
-      el =>
-        `<div class="gallery-item">
-            <a class="gallery-link" href="${el.largeImageURL}">
-                <img class="gallery-image" src="${el.webformatURL}" alt="${el.tags}" />
-            </a>
-            <div class="gallery-item-info">
-                <p class="gallery-item-info-par">
-                    <span class="gallery-item-info-span">Likes: <span>${el.likes}</span>
-                    </span>
-                </p>
-                <p class="gallery-item-info-par">
-                    <span class="gallery-item-info-span">Views: <span>${el.views}</span>
-                    </span>
-                </p>
-                <p class="gallery-item-info-par">
-                    <span class="gallery-item-info-span">Comments: <span>${el.comments}</span>
-                    </span>
-                </p>
-                <p class="gallery-item-info-par">
-                    <span class="gallery-item-info-span">Downloads: <span>${el.downloads}</span>
-                    </span>
-                </p>
-            </div>
-        </div>`
-    )
-    .join('');
-}
-
-function showEndOfListMessage() {
-  iziToast.info({
-    timeout: 3000,
-    position: 'topRight',
-    message: "We're sorry, but you've reached the end of search results.",
-  });
-}
-
-function showEmptyInputMessage() {
-  iziToast.info({
-    timeout: 3000,
-    position: 'topRight',
-    message: "The search query can not be empty!",
-  });
-}
-
-function noImagesMessage() {
-  iziToast.error({
-    timeout: 3000,
-    position: 'topRight',
-    message:
-      'There are no images matching your search query. Please, enter something else!',
-  });
-}
-
